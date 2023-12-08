021125c1b1b144d7f09e70074a09caccd8bdd0fb
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-12-08 00:47:27 -0800

    going back to platform.sh
    vercel, being serverless does not allows project to exceed 250MB

--- topicreport/settings.py
+++ topicreport/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     '.vercel.app',
+    '.platformsh.site'
 ]
 
 
@@ -123,8 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
