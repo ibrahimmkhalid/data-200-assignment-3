62afd3416205f61a556e266867a2230389f46082
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-12-07 20:23:09 -0800

    vercel configuration fix
    https://stackoverflow.com/questions/76066183/fixed-vercel-django-deployment-error-500-internal-server-error-unable-to-i

--- topicreport/topicreport/wsgi.py
+++ topicreport/topicreport/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'topicreport.settings')
 
+if os.environ.get('ENV') == 'production':
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 app = get_wsgi_application()
