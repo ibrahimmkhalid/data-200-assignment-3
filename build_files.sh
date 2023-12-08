72a82d1bfdf480ca2bfd48909dfdf7cb7faaf255
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-12-07 21:44:57 -0800

    update

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
-python -m venv venv
-source ./venv/bin/activate
+python3.10 -m venv venv
+source venv/bin/activate
 python -m pip install --upgrade pip
 pip install -r ./requirements.txt
 python manage.py collectstatic --noinput
