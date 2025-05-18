50a9eec73427affd1586e7f1d654682016d3ab5b
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2025-05-18 00:11:26 -0700

    update static

--- api/settings.py
+++ api/settings.py
@@ -126,8 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # Add other directories if needed
+]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
