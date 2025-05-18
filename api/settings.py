b2f14ba83fe8b8a4033dd90848e3064b6279c1aa
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2025-05-18 00:16:20 -0700

    update static

--- api/settings.py
+++ api/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = "/static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "core/static"),
     # Add other directories if needed
 ]
 
